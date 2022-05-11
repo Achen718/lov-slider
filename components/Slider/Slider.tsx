d3339034e2b2af783c7b345fc1f54021f48b8a28
achen718 <@alvin.chen.718@gmail.com>
2022-05-10 22:06:16 -0400

    Clean up

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,7 +1,8 @@
-
-import { useSprings, animated } from '@react-spring/web'
+import { useRef } from 'react'
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import sliderStyles from '../../styles/Slider.module.scss'
+import { useSprings, animated } from '@react-spring/web'
 
 interface Props {
   cityName: string;
@@ -14,30 +15,30 @@ const Slider: NextPage<Props> = ({ cityName, currentCity, description, creatures
   return (
     <div className={sliderStyles.container}>
       {/* header title / current locatio<div className="desc-section">*/}
-			<div className={sliderStyles.row}>
+			<div className={`${sliderStyles.row} ${sliderStyles.header}`}>
 				<div className={sliderStyles.col}>
-					{cityName}
+					<p className={sliderStyles.cityName}>{cityName}</p>
 				</div>
 				<div className={sliderStyles.col}>
-					{currentCity}
+					<button className={sliderStyles.currentCity}>{currentCity}</button>
 				</div>
 			</div>
 
-			{/* desc / level / creatures*/}
+			{/* desc / level / creatures, Add HR or border bottom to separate rows*/}
 			<div className={sliderStyles.row}>
 				{/* About */}
 				<div className={sliderStyles.col}>
-					<p className="title">About</p>
-					<p>{description}</p>
+					<p className="description-title">About</p>
+					<p className="description">{description}</p>
 				</div>
 				{/* Unlock level */}
 				<div className={sliderStyles.col}>
-					<p className="title">To Unlock</p>
-					<p>Level 'X' reach level 9 to unlock</p>
+					<p className="level">To Unlock</p>
+					<p>Level X reach level 9 to unlock</p>
 				</div>
 				{/* creatures */}
 				<div className={sliderStyles.col}>
-					<p className="title">Creatures</p>
+					<p className="creatures">Creatures</p>
 					<div>creature icons here</div>
 				</div>
 			</div>
